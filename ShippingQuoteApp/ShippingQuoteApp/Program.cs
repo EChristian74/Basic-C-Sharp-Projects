fa23acb98a928c863beaf75a736c896105617dfa
EChristian74 <75714720+EChristian74@users.noreply.github.com>
2021-10-22 12:10:16 -0500

    BRANCHING SUBMISSION ASSIGNMENT
    * Your job is to create a console-based application for getting a shipping quote for a package.
    * The first line of the program must be: “Welcome to Package Express. Please follow the instructions below.”
      - The user must then be prompted for the package weight.
    * If the weight is greater than 50, display the error message, “Package too heavy to be shipped via Package Express. Have a good day.” At this point the program would end.
      - The user must then be prompted for the package width.
      - Then the package height.
      - Then the package length.
    * If the dimensions total greater than 50, display the error message, “Package too big to be shipped via Package Express.” At this point the program would end.
      - Next, multiply the three dimensions (height, width, & length) together, and multiply the product by the weight. Finally, divide the outcome by 100.
      - The result of that calculation is the quote.
      - Display the quote to the user as a dollar amount.
    * Add comments to each line or block of your code to explain what it does exactly, so that another developer could read and understand your code.
    * Submitted 10/22/2021

---
+++ ShippingQuoteApp/ShippingQuoteApp/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShippingQuoteApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int maxWeight = 50;  //Total weight condition for shipping
+                        
+            //Shipping quote page greeting
+            Console.WriteLine("Welcome to Package Express. Please follow the instructions below.");
+
+            //First package inquiry - weight
+            Console.WriteLine("Please provide the weight of your package:");  //user entry
+            int packageWeight = Convert.ToInt32(Console.ReadLine());  //casting string to int
+
+            if (packageWeight > maxWeight)  //Total weight condition for shipping
+            {
+                Console.Write("Package too heavy to be shipped via Package Express. Have a good day.");  //Prints to console is weight exceeds maxWeight
+            }
+            Console.ReadLine();  //Pauses and holds open console for next section of program to run
+
+
+            int totalDimensions = 50;  //Total dimensions condition for shipping
+            int packageDivisor = 100;  //Variable used as divisor to final total width, height, length, weight calculation
+
+            //First package inquiry - width
+            Console.Write("Please provide package width: \n");
+            int packageWidth = Convert.ToInt32(Console.ReadLine());  //casting string to int
+
+            //First package inquiry - height
+            Console.Write("Please provide package height: \n");
+            int packageHeight = Convert.ToInt32(Console.ReadLine());  //casting string to int
+
+            //First package inquiry - length
+            Console.Write("Please provide package length:\n");
+            int packageLength = Convert.ToInt32(Console.ReadLine());  //casting string to int
+
+            if (packageWidth + packageHeight + packageLength > totalDimensions)  //Total dimensions condition for shipping
+            {
+                Console.Write("Package too big to be shipped via Package Express.");  //Prints to console is weight exceeds totalDimensions
+            }
+            else  //Condition if does not exceed totalDimensions 
+            {
+                //Calculates final shipping cost and prints to console
+                Console.Write("Your estimated total for shipping this package is: " + packageWidth * packageHeight * packageLength * maxWeight / packageDivisor +"\n");
+                Console.Write("Thank you!");  //Prints to console 
+            }
+            Console.ReadLine();  //Pauses and holds open console until user closes program
+
+
+        }
+    }
+}
