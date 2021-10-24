3c10ac46edaf4dbd5af1769b7bbb866cca14ba43
EChristian74 <75714720+EChristian74@users.noreply.github.com>
2021-10-24 16:38:37 -0500

    CONSOLE APP ARRAY SUBMISSION ASSIGNMENT:
    * Create a one-dimensional Array of strings. Ask the user to select an index of the Array and then display the string at that index on the screen.
    * Create a one-dimensional Array of integers. Ask the user to select an index of the Array and then display the integer at that index on the screen.
    * Add in a message that displays when the user selects an index that doesn’t exist.
    * Create a list of strings. Ask the user to select an index of the list and then display the content at that index on the screen.
    * Add comments to each line or block of your code to explain what it does exactly, so that another developer could read and understand your code.
    * Submitted 10/24/2021

---
+++ ArrayListProgramSecretIndexSelect/ArrayListProgramSecretIndexSelect/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+
+class Program
+{
+    static void Main()
+    {
+        //Program header
+        Console.WriteLine("Secret Password and Passcode Generator"); //prints to console
+        
+
+        //Initiates/instantiates new string array
+        string[] password = { "BileGful", "exREgIst", "nEOtaiNy", "strUcIst", "reINvoKy" };
+
+        //Question 1: String index for array
+        Console.WriteLine("Enter a number 0-4 to receive a secret password:"); //prints to console
+        int resp1 = int.Parse(Console.ReadLine());
+
+        if (resp1 > 4)  //condition to receive password
+        {
+            Console.WriteLine("Selection is invalid.");  //prints to console when condition not met
+        }
+        else
+        {
+            Console.WriteLine(password[resp1]);  //prints password to console when condition met
+        }
+        Console.ReadLine();  //pauses and holds open console for next section of program
+
+
+
+        //Initiates/instantiates new integer array
+        int[] passcode = { 6834, 2987, 3928, 3476, 8710 };
+
+        //Question 2: Integer Index for array
+        Console.WriteLine("Enter a number 0-4 to receive a secret passcode:"); //prints to console
+        int resp2 = int.Parse(Console.ReadLine());  // data entry
+
+        if (resp2 > 4)  //condition to receive passcode
+        {
+            Console.WriteLine("Selection is invalid.");  //prints to console when condition not met
+        }
+        else
+        {
+            Console.WriteLine(passcode[resp2]);  //prints passcode to console when condition met
+        }
+        Console.ReadLine();  //pauses and holds open console for next section of program
+
+
+        //Initiates/instantiates new list of strings
+        List<string> phrase = new List<string>();
+        phrase.Add("Cowa Bunga Dude");
+        phrase.Add("Fedora The Explorer");
+        phrase.Add("Kentucky Fried Chicks");
+        phrase.Add("Just Nuke It");
+        phrase.Add("Faster Than FedEx");
+
+        //Question 3: Integer index for list
+        Console.WriteLine("Enter a number 0-4 to receive a secret passphrase:"); //prints to console
+        int resp3 = int.Parse(Console.ReadLine());  // data entry
+
+        if (resp3 > 4)  //condition to receive phrase
+        {
+            Console.WriteLine("Selection is invalid.");  //prints to console when condition not met
+        }
+        else
+        {
+            Console.WriteLine(phrase[resp3]);  //prints phrase to console when condition met
+        }
+        Console.ReadLine();  //pauses and holds open console until user hits Return or exits console
+
+    }
+}
+
+        
+
+
+ 
