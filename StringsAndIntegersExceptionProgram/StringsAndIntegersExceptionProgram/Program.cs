13c85e2de2d128b3fd25ca6dafce038a5cb35db0
EChristian74 <75714720+EChristian74@users.noreply.github.com>
2021-10-30 17:40:37 -0500

    CONSOLE APP STRINGS AND INT SUB ASSIGNMENT
    Create a list of integers. Ask the user for a number to divide each number in the list by. Write a loop that takes each integer in the list, divides it by the number the user entered, and displays the result to the screen.
    
    * Run the code, entering in non-zero numbers as the user. Look at the displayed results.
    * Run the code again, entering in zero as the number to divide by. Note any error messages you get.
    * Run the code once again, entering in a string as the number to divide by. Note any error messages you get.
    * Now put the loop in a try/catch block. Below and outside of the try/catch block, make the program print a message to the display to let you know the program has emerged from the try/catch block and continued on with program execution. In the catch block, display the error message to the screen. Then try various combinations of user input: valid numbers, zero and a string. Ensure the proper error messages display on the screen, and that the code after the try/catch block gets executed.
    * Add comments to each line or block of your code to explain what it does exactly, so that another developer could read and understand your code.

---
+++ StringsAndIntegersExceptionProgram/StringsAndIntegersExceptionProgram/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Instantiation of and addition of string elements to a new list
+        List<int> intList = new List<int>() { 12, 25, 36, 50, 72, 100 };
+
+        Console.WriteLine("Division Program");  //Displays name of program to console      
+
+        foreach (int number in intList)  //For statement that result in iteration of each integer in list
+
+            //Try statement that houses all exception handling code
+            try
+            {
+                Console.WriteLine("Pick a number that will be used to divide each number in the list:");  //Instruction to user
+                int responseInt = Convert.ToInt32(Console.ReadLine());  //Casts user data entry
+                Console.WriteLine("Now dividing the the two...");  //Message that division will now take place, prints to console
+                int intResult = number / responseInt;  //Instruction to program to perform this math calculation
+                Console.WriteLine(number + " divided by " + responseInt + " equals " + intResult);  //Prints visual respresentation of math calculation
+                Console.ReadLine();  //Pauses and hold open program program for next section of code to run
+            }
+            //Catch 1: If user enters number other tha whole number 
+            catch (FormatException ex)  //Built-in exception specific to format
+            {
+                Console.WriteLine("Please type a whole number.");  //Prints message to use whole number to console
+                return;
+            }
+            //Catch 2: If user enters "0"
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("Please don't divide by zero.");
+            }
+            //Catch 3: General catch that allows for handling of other built-in exception errors
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);  //Catch clause exception handler that displays format exception message (i.e. string)
+            }
+            //Finally: Executes when the try/catch block leaves the execution - releases system resources
+            finally
+            {
+                Console.ReadLine();  //Pauses and hold open program program for next section of code to run
+            }
+        Console.WriteLine("The number(s) you have enters are valid.");  //Display message to console confirming numbers entered are valid
+        Console.ReadLine();  //Holds program open until user hits Return or exits the console
+
+    }
+}
+
+
+
