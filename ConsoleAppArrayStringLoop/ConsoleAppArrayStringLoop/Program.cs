2c2dc72993e44013c6422090a7d1dc6b27f9a435
EChristian74 <75714720+EChristian74@users.noreply.github.com>
2021-10-29 20:55:33 -0500

    CONSOLE APP SUBMISSION ASSIGNMENT 1-6
    * Create a console app that does/contains the following:
      - A one-dimensional array of strings.
      - Ask the user to input some text.
      - A loop that iterates through each string in the array and adds the user's text input to the end of each string. This step will not output anything to the console, but will     update each array element by appending the user's text.
      - Then create a loop that prints off each string in the array on a separate line.
    
    * An infinite loop.
      - Save your code.
      - Fix the infinite loop so that it will execute properly.
    
    * Add the following to your console app and perform these actions:
      - A loop where the comparison that’s used to determine whether to continue iterating the loop is a “<” operator.
      - Save and execute your code.
      - A loop where the comparison that’s used to determine whether to continue iterating the loop is a “<=” operator.
    
    * Add the following to your console app and perform these actions:
      - A list of strings where each item in the list is unique.
      - Ask the user to input text to search for in the list.
      - A loop that iterates through the list and then displays the index of the list that contains matching text on the screen.
      - Add code to the loop to check if the user put in text that isn't on the list and, if they did, tells the user their input is not on the list.
      - Add code to the loop that stops it from executing once a match has been found.
    
    * Add the following to your console app and perform these actions:
      - A list of strings that has at least two identical strings in the list. Ask the user to select text to search for in the list.
      - Create a loop that iterates through the list and then displays the indices of the items matching the user-selected text.
      - Add code to the loop to check if the user put in text that isn't on the list and, if they did, tells the user their input is not on the list.
    
    * Add the following to your console app and perform these actions:
      - Create a list of strings that has at least two identical strings in the list.
      - Create a foreach loop that evaluates each item in the list, and displays a message showing the string and whether or not it has already appeared in the list.
      - Save and execute your code.
    
    * Add comments to each line or block of your code to explain what it does exactly, so that another developer could read and understand your code.
    * Submitted 10/17/2021

---
+++ ConsoleAppArrayStringLoop/ConsoleAppArrayStringLoop/Program.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //ASSIGNMENT ONE/PART ONE: One-dimensional array of strings
+        string[] cities = { "Amarillo, ", "Lubbock, ", "Dallas, ", "San Antonio, ", "Houston, " };
+
+        //Opening statement providing user instruction that prints to console
+        Console.WriteLine("Please give the state abbreviation \nfor the cities of Amarillo, Lubbock, \nDallas, San Antonio, and Houston in all caps?");
+        string response1 = (Console.ReadLine());  //user data entry
+
+        for (int i = 0; i < cities.Length; i++)  //loop that iterates the array up to the number of index positions
+        {
+            cities[i] = cities[i] + response1;  //statement that concatenates original array strings and user input   
+            Console.WriteLine("Result (after joining) = " + cities[i]);  //Updated array of strings that prints to console
+        }
+        Console.ReadLine();  //holds open program for next section of code to run 
+
+
+        Console.WriteLine(" ");  //Simply inserts an extra blank line
+
+
+        //ASSIGNMENT TWO: Add infinite loop, then fix the loop so that it will execute properly
+        string[] websites = { "Google", "Facebook", "Twitter", "Instagram", "YouTube" };
+
+        //Opening statement providing user instruction that prints to console
+        Console.WriteLine("Press the Return button key on your \nkeyboard to see a list of internet browsers with \nletter \"o\" in the name.");
+        Console.ReadLine();  //Pauses program to display instruction to console
+
+        int j = 0;  //Creates variable assigned to index positions in array
+
+        while (j < websites.Length)  //Loop that iterates array of strings (browser names)
+        {
+            //Only output those sites with an 'o' in the name
+            if (websites[j].Contains("o"))  //Condition if the string contains the letter "o"
+            {
+                Console.WriteLine(websites[j]);  //prints browser names that meet condition to console
+            }
+            j++;  //Denotes that the loop will increment
+        }
+        Console.ReadLine();  //Holds open program for next section of code to run 
+
+
+
+        //ASSIGNMENT THREE(Part One): Comparison loop with less than operator
+
+        //While loop quiz asking of the top five favorite lucky number that people choose, which is #1
+        Console.Write("Choose a number less than 100 from which this loop will iterate downward. \n");
+        string response2 = Console.ReadLine();  //user data entry
+        int resp2 = Convert.ToInt32(response2);  //casting, string to integer  
+
+        Console.Write("");  //Blank line
+
+        while (resp2 < 100)  //loop that iterates from a number selected by user below 100
+        {
+            //Conditions that tells loop to count downward, not upward
+            Console.WriteLine(resp2);
+            resp2--;
+
+            //Conditions to stop loop once loop counts down to 1
+            if (resp2 < 1)
+                break;
+        }
+        Console.ReadLine();  //Holds open program for next section of code to run 
+
+
+
+        //ASSIGNMENT THREE (Part One): Comparison loop with less than or equal to operator
+        Console.WriteLine("Create multiplication tables based on a base number of your choosing time 1 to 20.");
+        Console.ReadLine();
+
+        Console.Write("Choose a base number:");  //Instruction to user
+        int TableOf = int.Parse(Console.ReadLine());  //user data entry
+
+        int TotalLength = 1;  //Creates variable assigned to index positions in array
+
+        while (TotalLength <= 20)  //Condition if that stops the loop once the base number is multiple by numbers 1-20
+        {
+            int TableValue = TotalLength * TableOf;  //Establishes the table structure
+            Console.WriteLine("{0} x {1} = {2}", TableOf, TotalLength, TableValue);  //Creates the condition
+            if (TotalLength == 10)
+            {
+                Console.WriteLine("\n");  //Simply write an extra blank line grouping tables in groups of 10
+            }
+            TotalLength++;  //Denotes that the loop will increment          
+        }
+        Console.ReadLine();  //Holds open program for next section of code to run 
+
+
+
+        //ASSIGNMENT FOUR: List of strings, user enters search text, looper interates for match,
+        //includes exception if search criteria not in list, then loop stops once a match is found
+
+        //Opening statement that informs user regarding the purpose of this program
+        Console.WriteLine("Can you guess any of the top five luxury supercars in the world?");
+
+        //Creation of and addition of string elements to a new list
+        List<string> cars = new List<string>() { "Porsche", "Ferrari", "Maserati", "Aston Martin", "Alfa Romeo" };
+
+        Console.WriteLine(" ");  //Blank line
+
+        Console.WriteLine("Enter your guess: \n");  //Instruction to user to enter a guess
+        string response3 = (Console.ReadLine());  //User data entry
+
+        foreach (string car in cars)  //Condition to iterate all strings in list
+        {
+            if (response3 == car)  //Condition to match user input against strings in list 
+            {
+                //Condition to write the index # and the actual string if a match is found
+                Console.WriteLine("That brand is in the top 5!");  //Prints phrase to console when met
+
+                int index = cars.IndexOf(response3);  //
+                Console.WriteLine($"{index}");  //When a match occurs, prints index # and correlating string to console
+                break;  //Tells program to break if condition met
+            }
+            else
+            {
+                //Message to inform if the user guess is incorrect
+                Console.WriteLine("That brand is not in the top 5.");  //Prints phrase to console when not met for each list item
+            }
+        }
+        Console.ReadLine();  //Holds open program for next section of code to run 
+
+
+
+
+        //ASSIGNMENT FIVE: List of strings with two identical strings in list, asks user to search list,
+        //loop interates through list, displays indices of matching items, loop that checks user input
+
+        //Opening statements that informs user regarding the purpose of this program
+        Console.WriteLine("In my home I have many TV\'s made by Sony, Samsung, Toshiba, Life is Good, and Roku, two of which are the same brand.");
+        Console.WriteLine("Of all my TV\'s, can you guess of which brand that I have more than one TV?");
+
+        //Creation of and addition of string elements to a new list
+        List<string> brands = new List<string>() { "Sony", "Samsung", "Toshiba", "Life is Good", "Roku", "Samsung" };
+
+        Console.WriteLine("Enter your guess: \n");  //Instruction to user to enter a guess
+        string response4 = (Console.ReadLine());  //User data entry
+
+        for (int i = 0; i < brands.Count; i++)  //Instruction to iterate all strings in list
+        {
+            if (brands[i] == response4)  //Condition if user data entry matches string in list
+            {
+                Console.WriteLine("Match found at index " + i);  //Writes message to console confirming a match
+            }
+            if (!brands.Contains(response4))  //Condition if user data entry does not match string in list
+            {
+                Console.WriteLine("Match not in the list.");  //Write message to console confirming not a match
+            }
+        Console.ReadLine();  //Pauses and holds open console until user hits Return or exits console
+
+
+
+            //ASSIGNMENT SIX: List of strings with two identical strings in list, asks user to search list,
+            //loop interates through list, displays indices of matching items, loop that checks user input
+
+            //Opening statements that informs user regarding the purpose of this program
+            Console.WriteLine("The students in my small classroom are named John, Sue, Bobby, Patricia, Frederick, and Pepe.");
+            Console.WriteLine("Of all student names in my classroom, can you guess which name occurs more than once?");
+
+            //Creation of and addition of string elements to a new list
+            List<string> students = new List<string>() { "John", "Sue", "Bobby", "Patricia", "Frederick", "Pepe", "Sue" };
+
+            Console.WriteLine(" ");  //Blank line
+
+            Console.WriteLine("Enter your guess: \n");  //Instruction to user to enter a guess
+            string response5 = (Console.ReadLine());  //User data entry
+            List<string> students2 = new List<string>();  //Creates empty list 
+
+            foreach (string student in students)  //Condition for list iteration
+            {
+                if (!students2.Contains(student))   //Condition if data entry is not a match
+                {
+                    students2.Add(student);  //Writes data to new list
+                    Console.WriteLine(student + " was not in the list.");  //Prints message to console when condition not met
+                }
+                else if (students2.Contains(student))  //Condition if data entry is a match
+                {
+                    //Message to inform if the user guess is incorrect
+                    Console.WriteLine(student + " was on the list.");  //Prints phrase to console when condition is met
+                }
+            }
+            //Console.ReadLine();  //Pauses and holds open console until user hits return or exits console
+
+        }
+    }
+}
+
+
+
--- WhileandDoWhileLoop/WhileandDoWhileLoop/Program.cs
+++ WhileandDoWhileLoop/WhileandDoWhileLoop/Program.cs
@@ -10,38 +10,38 @@ namespace WhileandDoWhileLoop
     {
         static void Main(string[] args)
         {
-            //While loop quiz asking of the top five favorite lucky number that people choose, which is #1
-            Console.WriteLine("Out of the top 3 numbers people consider to be lucky, which is #1?");  //question one, prints to console
-            int fnumber = Convert.ToInt32(Console.ReadLine());  //user data entry - guess lucky number
-            bool guess1 = fnumber == 32;  //Comparison condition to compare against user data entry
+            ////While loop quiz asking of the top five favorite lucky number that people choose, which is #1
+            //Console.WriteLine("Out of the top 3 numbers people consider to be lucky, which is #1?");  //question one, prints to console
+            //int fnumber = Convert.ToInt32(Console.ReadLine());  //user data entry - guess lucky number
+            //bool guess1 = fnumber == 32;  //Comparison condition to compare against user data entry
 
-            //While loop using switch statement
-            while (!guess1)
-            {
-                switch (fnumber)  //Switch statement that iterates through multiple choices for this quiz
-                {
-                    case 3:  //one of three multiple choices
-                        Console.WriteLine("You guessed 3. Try again.");  //prints to console
-                        Console.Write("Guess another number?");  //prints to console
-                        fnumber = Convert.ToInt32(Console.ReadLine());  //user data entry
-                        break;  //break if condition occurs
-                    case 8:  //two of three multiple choices
-                        Console.WriteLine("You guessed 8. Try again.");  //prints to console
-                        Console.Write("Guess another number?");  //prints to console
-                        fnumber = Convert.ToInt32(Console.ReadLine());  //user data entry
-                        break;  //break if condition occurs
-                    case 7:  //three of three multiple choices, the correct choice
-                        Console.WriteLine("You guessed the number 7. That is correct!");  //prints to console if true
-                        guess1 = true;
-                        break;  //break if condition occurs
-                    default:
-                        Console.WriteLine("That number is not in the top 3!");  //prints to console
-                        Console.Write("Guess another lucky number?");  //prints to console
-                        fnumber = Convert.ToInt32(Console.ReadLine());  //user data entry
-                        break;  //break if condition occurs
-                }
-            }
-            Console.ReadLine();  //pauses and holds program open for next section of program to run
+            ////While loop using switch statement
+            //while (!guess1)
+            //{
+            //    switch (fnumber)  //Switch statement that iterates through multiple choices for this quiz
+            //    {
+            //        case 3:  //one of three multiple choices
+            //            Console.WriteLine("You guessed 3. Try again.");  //prints to console
+            //            Console.Write("Guess another number?");  //prints to console
+            //            fnumber = Convert.ToInt32(Console.ReadLine());  //user data entry
+            //            break;  //break if condition occurs
+            //        case 8:  //two of three multiple choices
+            //            Console.WriteLine("You guessed 8. Try again.");  //prints to console
+            //            Console.Write("Guess another number?");  //prints to console
+            //            fnumber = Convert.ToInt32(Console.ReadLine());  //user data entry
+            //            break;  //break if condition occurs
+            //        case 7:  //three of three multiple choices, the correct choice
+            //            Console.WriteLine("You guessed the number 7. That is correct!");  //prints to console if true
+            //            guess1 = true;
+            //            break;  //break if condition occurs
+            //        default:
+            //            Console.WriteLine("That number is not in the top 3!");  //prints to console
+            //            Console.Write("Guess another lucky number?");  //prints to console
+            //            fnumber = Convert.ToInt32(Console.ReadLine());  //user data entry
+            //            break;  //break if condition occurs
+            //    }
+            //}
+            //Console.ReadLine();  //pauses and holds program open for next section of program to run
 
 
 
