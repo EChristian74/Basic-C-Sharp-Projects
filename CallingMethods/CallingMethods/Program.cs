fb9748a4640f090e06b01163d1361ed4c06341cf
EChristian74 <75714720+EChristian74@users.noreply.github.com>
2021-10-31 13:52:48 -0500

    CALLING METHODS SUBMISSION ASSIGNMENT:
    * Create a class. In that class, create three methods, each of which will take one integer parameter in and return an integer. The methods should do some       math operation on the received parameter. Put this class in a separate .cs file in the application.
    * In the Main() program, ask the user what number they want to do the math operations on.
    * Call each method in turn, passing the user input to the method. Display the returned integer to the screen.
    * Add comments to each line or block of your code to explain what it does exactly, so that another developer could read and understand your code.
    * Submitted 10/31/2021

---
+++ CallingMethods/CallingMethods/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethods
+{
+    class Program
+    {
+        public static int Data { get; set; }
+        static void Main(string[] args)
+        {
+            //heading of program - displays to console
+            Console.WriteLine("PLUS, MINUS, DIVIDE!");  
+
+            //instruction to user to enter a number to perform math operations
+            Console.WriteLine("Please enter a number that will be used \nto add, subtract, multiply, and divide:");
+            string line = Console.ReadLine();  //program reads user data entry
+            Data = Int32.Parse(line);  //converts string representation of a number to its 32-bit signed integer equivalent
+
+            var operatorObject = new Operator();  //instantiates new variable operatorObject  
+            var result = operatorObject.GetAdd(Data);  //calls on the class of Operator in Operator.cs file
+
+            Console.WriteLine(result);  //displays
+            Console.ReadLine();  //hold program open until user hits Return or exits the console
+        }
+
+    }
+}
