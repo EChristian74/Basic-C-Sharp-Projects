690ec93ec998e51a6c4cd768b09af01e5b9d8b9a
EChristian74 <75714720+EChristian74@users.noreply.github.com>
2021-11-02 20:53:03 -0500

    MAIN METHOD SUBMISSION ASSIGNMENT
    - Create a class. In that class, create a method that will take in an integer, create a math operation for this integer (addition, subtraction, etc.), then return the answer as an integer
    - In the Main() method of the console app, instantiate the class and call the one method, passing in an integer. Display the result to the screen.
    - Add a second method to the class with the same name that will take in a decimal, create a different math operation for it, then return the answer as an integer.
    - In the Main() method of the console app, instantiate the class and call the second method, passing in a decimal. Display the result to the screen.
    - Add a third method to the class, with the same name, that will take in a string, convert it to an integer if possible, do a different math operation to it, then return the answer as an integer.
    - In the Main() method of the console app, instantiate the class and call the third method, passing in a string that equates to an integer. Display the result to the screen.
    - Add comments to each line or block of your code to explain what it does exactly, so that another developer could read and understand your code.

---
+++ MainMethod/MainMethod/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethod
+{
+    class Program
+    {
+       
+
+        public static void Main(string[] args)
+        {
+
+            var operatorObject = new Operations();  //Creates variable that is used for all three methods
+
+            Console.WriteLine("Please enter a whole number: ");  //Instructs user to enter a number
+            int dataEntry1 = Convert.ToInt32(Console.ReadLine());  //Program reads user data entry and sets entry data type
+            var result1 = operatorObject.Add(dataEntry1);  //Calls method Add 
+            Console.WriteLine("The result of the method is " + result1);  //Writes result statement to console
+
+            Console.WriteLine();  //Blank line
+
+            Console.WriteLine("Please enter a decimal: ");  //Instructs user to enter a decimal
+            decimal dataEntry2 = Convert.ToDecimal(Console.ReadLine());  //Program reads user data entry and sets entry data type
+            int result2 = operatorObject.Dec(dataEntry2);  //Calls method Dec
+            Console.WriteLine("The result of the method is " + result2);  //Writes result statement to console
+
+            Console.WriteLine();  //Blank line
+
+            Console.WriteLine("Please enter a numeric string: ");  //Instructs user to enter a string
+            string dataEntry3 = Console.ReadLine();  //Program reads user data entry and sets entry data entry (string) - Console.ReadLine() default data type is string
+            int result3 = operatorObject.Integer(dataEntry3);  //Calls method Integer
+            Console.WriteLine("The result of the method is " + result3);  //Writes result statement to console
+
+            Console.ReadLine();  //Pauses and holds open console until user hits Return or Exits console
+
+
+            
+
+            
+
+        }
+    }
+}
