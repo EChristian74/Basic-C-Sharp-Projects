5b93de34d18461df997c46c7df4d15d71a451c1f
EChristian74 <75714720+EChristian74@users.noreply.github.com>
2021-11-03 19:57:54 -0500

    METHOD CLASS SUBMISSION ASSIGNMENT
    Create a class. In that class, create a void method that takes two integers as parameters. Have the method do a math operation on the first integer and display the second integer to the screen.
    
    In the Main() method of the console app, instantiate the class.
    
    Call the method in the class, passing in two numbers.
    
    Call the method in the class, specifying the parameters by name.
    
    Add comments to each line or block of your code to explain what it does exactly, so that another developer could read and understand your code.

---
+++ MethodClassVoid/MethodClassVoid/Method.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassVoid
+{
+    class Method  //Class named Method that will be called by Program.cs
+    {
+        public void Operation(int a, int b)  //Void method that takes in two user entry integer parameters
+        {
+            a = a + 20;  //Performs math operation of user entry (a) plus 20 - NOTE: DOES NOT PRINT TO CONSOLE
+            Console.WriteLine(b);  //Prints user entry (b) to console
+        }
+    }
+}
---
+++ MethodClassVoid/MethodClassVoid/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassVoid
+{
+    class Program  //Main program
+    {
+        static void Main(string[] args)
+        {
+            var Object = new Method();  //Creates object that is used by the class Method of the same name
+
+            Console.WriteLine("Please enter an integer: ");  //Instructs user to enter a number
+            int a = Convert.ToInt32(Console.ReadLine());  //Program reads user data entry and sets entry data type
+       
+            Console.ReadLine();  //Blank line
+
+            Console.WriteLine("Please enter another integer: ");  //Instructs user to enter a number
+            int b = Convert.ToInt32(Console.ReadLine());  //Program reads user data entry and sets entry data type
+
+            Console.ReadLine();  //Blank line
+
+            Console.WriteLine("The result of parameter \"b\": ");  //Prints only the result of parameter "b" in Method.cs
+            Object.Operation(a, b);
+
+            Console.ReadLine();  //Holds open console until user hits Return or exits the console
+
+        }
+    }
+}
