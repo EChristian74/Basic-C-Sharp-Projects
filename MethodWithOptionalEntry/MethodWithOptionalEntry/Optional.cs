e19c4eae178782ef5fdd740a2dc152796227f665
EChristian74 <75714720+EChristian74@users.noreply.github.com>
2021-11-04 18:21:45 -0500

    METHOD SUBMISSION ASSIGNMENT
    Create a class. In that class, create a method that takes two integers as parameters. Make one of them optional. Have the method do a math operation and return an integer result.
    
    In the Main() method of the console app, instantiate the class.
    
    Ask the user to input two numbers, one at a time. Let them know they need not enter anything for the second number.
    
    Call the method in the class, passing in the one or two numbers entered.
    
    Try various combinations of numbers on the code, including having no second number.
    
    Add comments to each line or block of your code to explain what it does exactly, so that another developer could read and understand your code.

---
+++ MethodWithOptionalEntry/MethodWithOptionalEntry/Optional.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodWithOptionalEntry
+{
+    class Method  //Class named Method that will be called by Program.cs 
+    {
+        public int Optional(int x, int y = 0)  //Public method that takes in two user entry parameters 
+        {
+            return x + y;  //Terminates the execution of the method in which it appears and returns the control back to the calling method
+        }
+    }
+}
---
+++ MethodWithOptionalEntry/MethodWithOptionalEntry/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodWithOptionalEntry
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var testObject = new Method();  //Creates variable that is used for all three methods
+
+            //int discount = (int).02;
+
+            Console.WriteLine("AGE DISCOUNT APPLICATION");  //Program Heading
+
+            Console.WriteLine("Please enter the number of years driving for one or more drivers in your household:");  //Info line
+
+            Console.WriteLine("Please enter the number of years driving for Driver 1: ");  //Instructs user to enter a number
+            int x = Convert.ToInt32(Console.ReadLine());  //Program reads user data entry and sets entry data type
+            int result1 = testObject.Optional(x);  //Calls method Optional
+
+            Console.ReadLine();  //Blank line
+
+            Console.WriteLine("Please enter the number of years driving for Driver 2 (optional, \nenter zero if you do not wish to enter: ");  //Instructs user to enter a number
+            int y = Convert.ToInt32(Console.ReadLine());  //Program reads user data entry and sets entry data type
+            int result2 = testObject.Optional(y);  //Calls method Optional
+
+            Console.ReadLine();  //Blank line
+
+            Console.WriteLine("The number of years on which we will base your discount is: ");  //Prints final communication statement to console
+
+            Console.WriteLine(testObject.Optional(x, y));  //Prints final match operation result to the console
+
+            Console.ReadLine();  //Holds open console until user hits Return or exits the console
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
