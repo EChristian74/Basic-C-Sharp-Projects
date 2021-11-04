346b59e61763872f19e9beccafbfefd2876a3bfe
EChristian74 <75714720+EChristian74@users.noreply.github.com>
2021-11-04 18:18:34 -0500

    Update Main Method
    Minor updates.

--- MainMethod/MainMethod/Operations.cs
+++ MainMethod/MainMethod/Operations.cs
@@ -13,21 +13,21 @@ namespace MainMethod
         public int Add(int data)  //public int = output | Add(int data) = data entry input
         {
             //No need to convert data type of input due to integer to integer exercise
-            return data + 5;  //method operation (addition)
+            return data + 5;  //terminates the execution of the method in which it appears and returns the control back to the calling method
         }
 
         //Method 2: Dec
         public int Dec(decimal data)  //public int = output | Dec(decimal data data) = data entry input
         {
             int x = Convert.ToInt32(data);  //converts data type of input from decimal to integer
-            return x * 5;  //method operation (multiplication)
+            return x * 5;  //terminates the execution of the method in which it appears and returns the control back to the calling method
         }
 
         //Method 3: Integer
         public int Integer(string data)  //public int = output | Integer(int data) = data entry input
         {
             int y = Convert.ToInt32(data); //converts data type of input from string to integer
-            return y / 5;  //method operation (division)
+            return y / 5;  //terminates the execution of the method in which it appears and returns the control back to the calling method
         }
 
 
