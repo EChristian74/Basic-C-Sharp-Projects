1d2be829638cf21cf1ad5a7d1039c7ee06f5699e
EChristian74 <75714720+EChristian74@users.noreply.github.com>
2021-11-05 18:34:34 -0500

    METHOD SUBMISSION ASSIGNMENT
    Final corrections.

---
+++ MethodWithOptionalEntry/MethodWithOptionalEntry/Option.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodWithOptionalEntry
+{
+    class Option  //Class named Method that will be called by Program.cs 
+    {
+        public int Operation(int x, int y=1)  //Public method that takes in two user entry parameters 
+        {
+            int result = x * y;  //Operation multiplying both parameters
+            return result;  //Terminates execution of the method and returns control to the calling method
+        }
+    }
+}
+
--- MethodWithOptionalEntry/MethodWithOptionalEntry/Program.cs
+++ MethodWithOptionalEntry/MethodWithOptionalEntry/Program.cs
@@ -11,9 +11,8 @@ namespace MethodWithOptionalEntry
         static void Main(string[] args)
         {
 
-            var testObject = new Method();  //Creates variable that is used for all three methods
-
-            //int discount = (int).02;
+            Option testObject = new Option();  //Creates variable that is used for all three methods
+          
 
             Console.WriteLine("AGE DISCOUNT APPLICATION");  //Program Heading
 
@@ -21,24 +20,26 @@ namespace MethodWithOptionalEntry
 
             Console.WriteLine("Please enter the number of years driving for Driver 1: ");  //Instructs user to enter a number
             int x = Convert.ToInt32(Console.ReadLine());  //Program reads user data entry and sets entry data type
-            int result1 = testObject.Optional(x);  //Calls method Optional
-
-            Console.ReadLine();  //Blank line
+           
 
             Console.WriteLine("Please enter the number of years driving for Driver 2 (optional, \nenter zero if you do not wish to enter: ");  //Instructs user to enter a number
-            int y = Convert.ToInt32(Console.ReadLine());  //Program reads user data entry and sets entry data type
-            int result2 = testObject.Optional(y);  //Calls method Optional
+            string y = Console.ReadLine();  //Program reads user data entry and sets entry data type
 
-            Console.ReadLine();  //Blank line
-
-            Console.WriteLine("The number of years on which we will base your discount is: ");  //Prints final communication statement to console
-
-            Console.WriteLine(testObject.Optional(x, y));  //Prints final match operation result to the console
+            if (y == "")  //Condition if y is blank, execute the below
+            {
+                Console.WriteLine("The number of years on which we will base your discount is: " + testObject.Operation(x));  //Program will execute running only the x parameter of the method Option
+                Console.ReadLine();  //Holds open console until user hits Return or exits the console
+            }
+            else  //Condition if y not blank, execute the below
+            {
+                int z = Convert.ToInt32(y);  //Casting of y from string to integer
+                Console.WriteLine("The number of years on which we will base your discount is: " + testObject.Operation(x, z));  //Program will execute running both x and y of the method Option
+            }
 
             Console.ReadLine();  //Holds open console until user hits Return or exits the console
 
 
-
+            
 
 
 
