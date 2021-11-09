2388e47ead06a4b872e6e3fd030b487624c3f6b6
EChristian74 <75714720+EChristian74@users.noreply.github.com>
2021-11-08 20:26:26 -0600

    OPERATORS SUBMISSION ASSIGNMENT
    * Overload the “==” operator so it checks if two Employee objects are equal by comparing their Id property.
    * Add comments to each line or block of your code to explain what it does exactly, so that another developer could read and understand your code.

---
+++ OverloadOperator/OverloadOperator/Employee.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverloadOperator
+{
+    public class Employee //Class Employee
+    {
+        public int EmployeeID { get; set; }  //Creates property employeeID
+        public string EmployeeName { get; set; }  //Creates property firstName
+
+        //Overload operator method using equals to compare employee ID's
+        public static bool operator ==(Employee employee1, Employee employee2)
+        {
+            //Condition that tests if ID's are equal
+            if (employee1.EmployeeID.Equals(employee2.EmployeeID)) 
+            {
+                return true; //return statement returns true if condition met (Equals)
+            }
+            else
+            {
+                return false; //return statement returns false if condition not met (Equals)
+            }
+        }
+
+        //Overload operator method using does not equal to compare employee ID's
+        public static bool operator !=(Employee employee1, Employee employee2) 
+        {
+            if (!employee1.EmployeeID.Equals(employee2.EmployeeID))
+            {
+                return false; //return statement returns false if condition met (Does Not Equal)
+            }
+            else
+            {
+                return true; //return statement returns true if condition not met (Does Not Equal)
+            }
+        }
+
+    }
+    }
---
+++ OverloadOperator/OverloadOperator/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverloadOperator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee1 = new Employee(); //Creates object for employee1
+            employee1.EmployeeID = 1234; //Assigns a value to EmployeeId
+            employee1.EmployeeName = "John Smith"; //Assigns a value to EmployeeName
+            string emp1 = employee1.EmployeeID + " " + employee1.EmployeeName; //Concatenates ID and Name
+            Console.WriteLine(emp1); //Writes the above concatenated string to console
+            Console.ReadLine();  //Holds open console for next section of code to run
+
+            Employee employee2 = new Employee();  //Creates object for employee2
+            employee2.EmployeeID = 5678;  //Assigns a value to EmployeeID
+            employee2.EmployeeName = "Fred Jones"; //Assigns a value to EmployeeName
+            string emp2 = employee2.EmployeeID + " " + employee2.EmployeeName; //Concatenates ID and Name
+            Console.WriteLine(emp2); //Writes the above concatenated string to console
+            Console.ReadLine(); //Holds open console for next section of code to run 
+
+            //QC check #1 to ensure method overloading (True)
+            Console.WriteLine(employee1.EmployeeID == employee2.EmployeeID); //Writes comparison result to console 
+            Console.ReadLine(); //Holds open console for next section of code to run 
+
+            //QC check #2 to ensure method overloading (False)
+            Console.WriteLine(employee1.EmployeeID != employee2.EmployeeID); //Writes comparison result to console 
+            Console.ReadLine(); //Holds open console for user to either hit Return or Exit console
+
+        }
+    }
+}
