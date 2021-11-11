a319f1e7ae7ce17816ee24805237dc8a1c5dda5e
EChristian74 <75714720+EChristian74@users.noreply.github.com>
2021-11-10 20:01:26 -0600

    PARSING ENUMS SUBMISSION ASSIGNMENT
    * Create an enum for the days of the week.
    * Prompt the user to enter the current day of the week.
    * Assign the value to a variable of that enum data type you just created.
    * Wrap the above statement in a try/catch block and have it print "Please enter an actual day of the week.” to the console if an error occurs.
    * Add comments to each line or block of your code to explain what it does exactly, so that another developer could read and understand your code.

---
+++ Enums/Enums/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Enums
+{
+    class Program
+    {
+        
+        public enum EDay //Collection of enums for program
+        {
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+        }
+
+        static void Main(string[] args)
+        {
+            
+            EDay value = EDay.Wednesday; //Created enum value to validate against condition in if statement
+
+            Console.WriteLine("Please enter the current day of the week."); //Opening instruction to user
+            string currentday = Console.ReadLine(); //User data entry
+            EDay day; //Assignment of enum to a variable
+
+            try
+            {
+                day = (EDay)Enum.Parse(typeof(EDay), currentday); //Try statement to parse/validate data entry against + converts data type
+
+            }
+            catch (Exception ex) //Assignment of exception to a variable
+            {
+                Console.WriteLine("Please enter an actual day of the week."); //Instruction to user if data entry does not pass try pass
+                Console.WriteLine(ex.Message); //Prints instruction to console to direct user to make another attempt
+            }
+            Console.ReadLine(); //Holds program open for next section of code to run 
+
+            if (value == EDay.Wednesday) //Condition that states if the data entered by user is "Wednesday", condition is met
+            {
+                Console.WriteLine("Yes, today is Wednesday?"); //Prints acknowledgment to user that condition is met
+            }
+            Console.ReadLine(); //Holds program open until user executes Return or exits console
+        }
+ 
+    }
+}
