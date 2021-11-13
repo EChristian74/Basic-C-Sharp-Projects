6059eb191be8ede0ad18a64845348fdb375d22f2
EChristian74 <75714720+EChristian74@users.noreply.github.com>
2021-11-13 15:11:45 -0600

    LAMBDA SUBMISSION ASSIGNMENT
    * In the Main() method, create a list of at least 10 employees. Each employee should have a first and last name, as well as an Id. At least two employees should have the first name “Joe”.
    * Using a foreach loop, create a new list of all employees with the first name “Joe”.
    * Perform the same action again, but this time with a lambda expression.
    * Using a lambda expression, make a list of all employees with an Id number greater than 5.
    * Add comments to each line or block of your code to explain what it does exactly, so that another developer could read and understand your code.

---
+++ LambdaExpression/LambdaExpression/Employee.cs
@@ -0,0 +1,15 @@
+﻿namespace LambdaExpression
+{
+    internal class Employee
+    {
+        public Employee() //Class Employee used to 
+        {
+        }
+
+        //Get and set allows us to creating properties for a class ini C# without having 
+        //to define private variables for them
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
---
+++ LambdaExpression/LambdaExpression/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LambdaExpression
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<Employee> empList = new List<Employee>(); //List is an object which holds objects in a specific order
+
+            empList.Add(new Employee() //Adds Employee object to list
+            {
+                Id = 1, FirstName = "Fred", LastName = "Wilson"  //Individual Employee object attributes
+            });
+            empList.Add(new Employee() //Adds Employee object to list
+            {
+                Id = 2, FirstName = "Louis", LastName = "Anderson" //Individual Employee object attributes
+            });
+            empList.Add(new Employee() //Adds Employee object to list
+            {
+                Id = 3, FirstName = "Joe", LastName = "McClean" //Individual Employee object attributes
+            });
+            empList.Add(new Employee() //Adds Employee object to list
+            {
+                Id = 4, FirstName = "Susan", LastName = "Bahnsen" //Individual Employee object attributes
+            });
+            empList.Add(new Employee() //Adds Employee object to list
+            {
+                Id = 5, FirstName = "Barbara", LastName = "Strickland" //Individual Employee object attributes
+            });
+            empList.Add(new Employee() //Adds Employee object to list
+            {
+                Id = 6, FirstName = "Frederick", LastName = "Peters" //Individual Employee object attributes
+            });
+            empList.Add(new Employee() //Adds Employee object to list
+            {
+                Id = 7, FirstName = "Joe", LastName = "McCarthur" //Individual Employee object attributes
+            });
+            empList.Add(new Employee() //Adds Employee object to list
+            {
+                Id = 8, FirstName = "Frances", LastName = "Johanson" //Individual Employee object attributes
+            });
+            empList.Add(new Employee() //Adds Employee object to list
+            {
+                Id = 9, FirstName = "Louie", LastName = "Salvatore" //Individual Employee object attributes
+            });
+            empList.Add(new Employee() //Adds Employee object to list
+            {
+                Id = 10, FirstName = "Kelly", LastName = "Clarkson" //Individual Employee object attributes
+            });
+
+            //Loop that iterates all objects in list assigning the keyword "data" to objects
+            foreach (var data in empList)
+            {
+                if (data.FirstName == "Joe") 
+                //Prints Employee Id, FirstName, LastName as a vertical list to console for objects with the first name "Joe"
+                Console.WriteLine("Id: {0}, Name: {1} {2}", data.Id, data.FirstName, data.LastName);
+            }
+            Console.ReadLine(); //Holds open console for next section of code to run
+
+            
+            Console.WriteLine("Retrieving employees with an Id greater than 5\n");
+            //Lambda expression inserted into foreach loop to iterate list Employee
+            //to identify all Id's > 5 assigning the keyword "data" to objects
+            foreach (Employee data in empList.FindAll(e => (e.Id > 5)).ToList())
+            {
+                //Prints Employee Id, FirstName, LastName as a vertical list to console for objects with an Id > 5
+                Console.WriteLine("Id: {0}, Name: {1} {2}", data.Id, data.FirstName, data.LastName);
+            }
+            Console.ReadLine(); //Holds open console until user executes Return or exits the console
+
+        }
+        
+    }
+}
