853414242c9cc2e21dfc814003796fa702df341a
EChristian74 <75714720+EChristian74@users.noreply.github.com>
2021-11-15 21:46:36 -0600

    INPUT ASSIGNMENT
    * Asks the user for a number.
    * Logs that number to a text file.
    * Prints the text file back to the user.
    * Add comments to each line or block of your code to explain what it does exactly, so that another developer could read and understand your code.

---
+++ InputWriteReadText/InputWriteReadText/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO; //"System.IO" required to write, read, append to a text file
+
+namespace InputWriteReadText
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please provide a number: "); //Instruction to user to provide a number
+            string entry = Console.ReadLine(); //Logs the user data entry
+            int num = Convert.ToInt32(entry); //Casts string data entry to integer
+            Console.ReadLine(); //Pauses and holds program open to run next section of program
+
+            //StreamWriter object = path of log = "using" statement relates to utilization of a memory resource
+            //Once the program hits the closing brace, the "using" statement enables the memory manager disposes of the resource
+            using (StreamWriter file = new StreamWriter(@"C:\Users\timek\Desktop\Basic_C#_Programs\VideoTutorialSeries\TwentyOneGameProgram\Logs\log.txt", true))
+            {
+                //References the variable "entry" above
+                file.WriteLine(entry);
+            } //Once the program hits this closing brace, the memory manager disposes of the resource
+
+            //String statement gathers all data written to the "log.txt" file | if the "@" symbol is not used, double backslashes are required
+            string text = File.ReadAllText(@"C:\Users\timek\Desktop\Basic_C#_Programs\VideoTutorialSeries\TwentyOneGameProgram\Logs\log.txt");
+            Console.WriteLine("The number you entered is: " + text); //Prints back to the user what number they entered
+            Console.ReadLine(); //Holds open the console until the user executes "Return" or exits the console
+        }
+    }
+}
+
