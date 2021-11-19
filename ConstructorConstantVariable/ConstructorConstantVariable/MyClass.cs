442e9196f487eb55a80cbec60a4c79792c10fa7e
EChristian74 <75714720+EChristian74@users.noreply.github.com>
2021-11-18 20:36:08 -0600

    CONSTRUCTOR SUBMISSION ASSIGNMENT
    * Create a const variable.
    * Create a variable using the keyword “var.”
    * Chain two constructors together.
    * Add comments to each line or block of your code to explain what it does exactly, so that another developer could read and understand your code.

---
+++ ConstructorConstantVariable/ConstructorConstantVariable/MyClass.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorConstantVariable
+{
+    public class NewEmployee //Class NewEmployee
+    {
+        public string firstName; //Declares property firstName
+        public string lastName; //Declares property lastName
+        
+        //This is a two parameter constructor method
+        //However, a default value is preset for lastName
+        //First constructor
+        public NewEmployee(string fName) : this(fName, "Boland")
+        {
+            
+        }
+        
+        //This is a two parameter constructor method
+        //Second constructor
+        public NewEmployee(string fName, string lName)
+        {
+            lastName = lName; //Instantiates lastName property
+            firstName = fName; //Instantiates firstName property
+        }   
+    }
+}
+
+
+
---
+++ ConstructorConstantVariable/ConstructorConstantVariable/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ConstructorConstantVariable
+{
+    class Programs
+    {
+
+        static void Main(string[] args)
+        {
+           
+            const string greeting = "Welcome new employee!";  //Const keyword string greeting
+            Console.WriteLine(greeting); //Writes greeting to console
+
+            Console.WriteLine("Please type in your first name."); //Instruction to user to provide first name
+            string name = Console.ReadLine(); //Declares variable of name and reads user input
+
+            var newEmployee = new NewEmployee(name); //Object for calling of NewEmployee class/method
+            Console.WriteLine("Welcome {0} {1}", newEmployee.firstName, newEmployee.lastName); //Message that incorporates string method to call NewEmployee parameters
+            Console.ReadLine(); //Holds open console until user executes the Return key or exits the console
+
+        }
+    }
+}
+
+
